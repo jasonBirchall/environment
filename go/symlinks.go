42a5e28f860edc3c723b78357e09d7d766a41f14
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2020-06-05 23:33:51 +0100

    Slight refactor and add populate map function

--- go/symlinks.go
+++ go/symlinks.go
@@ -1,28 +1,63 @@
+//TODO: sort out the existance checks
+//FIXME: Find out why the createSymlink isn't working
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
 func main() {
-	checkIfEnvSet("ENVIRONMENT")
-	checkIfEnvSet("DOTFILES")
-
-	// const (
-	// 	dotfiles    = os.LookupEnv("DOTFILES")
-	// 	environment = os.LookupEnv("ENVIRONMENT")
-	// )
-	// println(dotfiles, environment)
+	IsEnvReady()
+	PopulateMap()
 }
 
-func checkIfEnvSet(key string) {
+func IsEnvReady() {
+	CheckIfEnvSet("ENVIRONMENT")
+	CheckIfEnvSet("DOTFILES")
+}
+
+func CheckIfEnvSet(key string) bool {
 	value, exists := os.LookupEnv(key)
 	if !exists {
 		panic(key + " variable does not exist")
+	} else if _, err := os.Stat(value); os.IsNotExist(err) {
+		panic(value + " does not exist")
 	}
-	createConst(key, value)
+	return true
 }
 
-func createConst(key, value string) {
-	const key = value
+func PopulateMap() {
+	dotfiles, exists := os.LookupEnv("DOTFILES")
+	if !exists {
+		panic("Requried variable does not exist")
+	}
+
+	csvFile, _ := os.Open("files.csv")
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// CreateSymlink(dotfiles, line[0], line[1])
+
+		fmt.Println(dotfiles, line[0], line[1])
+	}
 }
+
+// func CreateSymlink(dir, source, target string) {
+// 	name := dir, source
+// 	println(dir)
+// 	println(source)
+// 	println(target)
+// }
