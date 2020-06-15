e9e993151c42334df933db52ff10f4766bf03bee
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2020-06-15 17:50:52 +0100

    Add function to perform ping every two seconds

--- go/ping/main.go
+++ go/ping/main.go
@@ -10,21 +10,27 @@ import (
 )
 
 func main() {
+	doEvery(20*time.Millisecond, ping)
+}
+
+// Idea taken from https://gist.github.com/ryanfitz/4191392
+func doEvery(duration time.Duration, function func(time.Time)) {
+	for time := range time.Tick(duration) {
+		function(time)
+	}
+}
+
+func ping(t time.Time) {
 	ping := fastping.NewPinger()
 	resolve, err := net.ResolveIPAddr("ip4:icmp", os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	// fmt.Println(resolve)
-	// fmt.Println(ping)
 
 	ping.AddIPAddr(resolve)
 	ping.OnRecv = func(address *net.IPAddr, rtt time.Duration) {
-		fmt.Println(rtt)
-	}
-	ping.OnIdle = func() {
-		fmt.Println("finish")
+		fmt.Println("ping:", rtt)
 	}
 
 	err = ping.Run()
